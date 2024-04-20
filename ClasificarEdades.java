cec2e8868b4e21c9e0d06a937d969e0fce238475
VictorF232 <137571008+VictorF232@users.noreply.github.com>
2024-04-19 20:36:14 -0400

    ClasificarEdades.java

--- ClasificarEdades.java
+++ ClasificarEdades.java
@@ -6,34 +6,35 @@ package com.mycompany.clasificaredades;
 
 import java.util.Scanner;
 
-public class ClasificarEdades {
+public class ClasificarEdades{
     
-    public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    int sumaMayor = 0;
-    int sumaMenor = 0;
-    int countMayor = 0;
-    int countMenor = 0;
-   
-    for (int i = 1; i <= 5; i++) {
-    System.out.print("Ingrese la edad de la persona " + i + ": ");
-        int edad = scanner.nextInt();
-        if (edad >= 18) {
-            sumaMayor += edad;
-            countMayor++;
-  } 
-        else {
-        sumaMenor += edad;
-        countMenor++;
-   }
-  }
-    double promedioMayores = countMayor > 0 ? (double) sumaMayor / countMayor : 0;
-    double promedioMenores = countMenor > 0 ? (double) sumaMenor / countMenor : 0;
-    
-    
-    System.out.println("Promedio de edades de mayores de edad: " + promedioMayores);
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        
+         int sumaMenorEdad = 0;
+        int sumaMayorEdad = 0;
+        int contadorMayor = 0;
+        int contadorMenor = 0;
+        
+        
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("Ingrese la edad de la persona " + i + ": ");
+            int edad = scanner.nextInt();
+            if(edad >= 18){
+            sumaMayorEdad += edad;
+            contadorMayor++;
+    } 
+            else{
+            sumaMenorEdad += edad;
+            contadorMenor++;
+     }
+    }
+    double promedioMenores = contadorMenor > 0 ? (double) sumaMenorEdad / contadorMenor : 0;
+    double promedioMayores = contadorMayor > 0 ? (double) sumaMayorEdad / contadorMayor : 0;
+        
     System.out.println("Promedio de edades de menores de edad: " + promedioMenores);
+        
+    System.out.println("Promedio de edades de mayores de edad: " + promedioMayores);
     scanner.close();
+    }
 }
-}
-
